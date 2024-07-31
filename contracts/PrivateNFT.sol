e996cf5ae4ee5d2f30827a402e90300cd521f9a3
Rahul h <jio2mobile9389@gmail.com>
2024-07-31 15:58:57 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("rahulh","rrrrrr")
+        ERC721("qwert","qwert")
         Ownable(initialOwner)
     {}
 
